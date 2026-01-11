3022d2e5fd971a6c9ea6aeda4dacf4b4c7c06790
João Guilherme <161392139+Glxhst@users.noreply.github.com>
2026-01-10 22:04:25 -0300

    Add files via upload

---
+++ concat.py
@@ -0,0 +1,10 @@
+# Concatenando Dados
+def concatenar_dados(lista1, lista2):
+
+    return lista1 + lista2
+
+# Exemplo de uso
+lista_a = [1, 2, 3]
+lista_b = [4, 5, 6]
+resultado = concatenar_dados(lista_a, lista_b)
+print("Lista concatenada:", resultado)
---
+++ operações.py
@@ -0,0 +1,7 @@
+# Operações matematicas básicas
+def somar(a, b):
+    return a + b
+
+n1 = (int(input("Digite o primeiro número para soma: ")))
+n2 = (int(input("Digite o segundo número para soma: ")))
+print("A soma é:", somar(n1, n2))
